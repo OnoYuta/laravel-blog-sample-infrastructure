79baa5e0e011be1f3d3dd0b098e5cf0b0591c0b6
ono <f25jzzydg@me.com>
2020-06-16 07:01:58 +0900

    Add public route table

--- vpc.tf
+++ vpc.tf
@@ -25,3 +25,21 @@ resource "aws_subnet" "public_subnet_2" {
     Name = "${var.project_name}-public_subnet_2"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "${var.project_name}-public-route-table"
+  }
+}

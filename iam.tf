71204b16a41cd5b85c78cc1fc688d8ad7f7ec275
ono <f25jzzydg@me.com>
2020-06-16 08:13:37 +0900

    Add ecs_instance_policy

--- iam.tf
+++ iam.tf
@@ -21,6 +21,18 @@ resource "aws_iam_role" "ecs_instance_role" {
 }
 
 resource "aws_iam_instance_profile" "ecs_instance_profile" {
-  name       = "ecs-instance-profile"
+  name = "ecs-instance-profile"
+  role = aws_iam_role.ecs_instance_role.name
+}
+
+resource "aws_iam_policy" "ecs_instance_policy" {
+  name        = "ecs-instance-policy"
+  path        = "/"
+  description = "ecs instance policy"
+  policy      = file("policies/ecs_instance_policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_attach" {
   role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = aws_iam_policy.ecs_instance_policy.arn
 }

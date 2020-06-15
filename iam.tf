ada4bc0418a608c11ca32e7d8244036a47531edd
ono <f25jzzydg@me.com>
2020-06-16 07:25:47 +0900

    Add ecs_instance_role

--- iam.tf
+++ iam.tf
@@ -13,3 +13,9 @@ resource "aws_iam_user_policy_attachment" "deploy-attach" {
   user       = aws_iam_user.deploy-user.name
   policy_arn = aws_iam_policy.deploy.arn
 }
+
+resource "aws_iam_role" "ecs_instance_role" {
+  name               = "ecs_instance_role"
+  path               = "/"
+  assume_role_policy = file("role_policies/ec2_assume_role_policy.json")
+}

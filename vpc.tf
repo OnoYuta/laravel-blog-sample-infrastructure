b3509a8abde224605649a51e046e2bffe5bb5e78
ono <f25jzzydg@me.com>
2020-06-16 06:33:50 +0900

    Add public subnet

--- vpc.tf
+++ vpc.tf
@@ -4,3 +4,15 @@ resource "aws_vpc" "vpc" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 }
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = "${var.region}a"
+  cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 8, 0)
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = "${var.region}c"
+  cidr_block        = cidrsubnet(aws_vpc.vpc.cidr_block, 8, 16)
+}

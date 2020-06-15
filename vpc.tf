ad7a19744e7def4f6b285da0404290c004eeae0d
ono <f25jzzydg@me.com>
2020-06-16 07:06:54 +0900

    Add public subnet route table association

--- vpc.tf
+++ vpc.tf
@@ -43,3 +43,13 @@ resource "aws_route_table" "public_route_table" {
     Name = "${var.project_name}-public-route-table"
   }
 }
+
+resource "aws_route_table_association" "public_subnet_1_route_table_association" {
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public_subnet_1.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_route_table_association" {
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public_subnet_2.id
+}

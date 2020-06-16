4b93ceb7e07678215c84f7a8e098b1a1fa1e26f6
ono <f25jzzydg@me.com>
2020-06-17 08:33:25 +0900

    Add IAM role for ecs task

--- iam.tf
+++ iam.tf
@@ -36,3 +36,9 @@ resource "aws_iam_role_policy_attachment" "ecs_instance_role_attach" {
   role       = aws_iam_role.ecs_instance_role.name
   policy_arn = aws_iam_policy.ecs_instance_policy.arn
 }
+
+resource "aws_iam_role" "ecs_task_role" {
+  name               = "ecs-task-role"
+  path               = "/"
+  assume_role_policy = file("role_policies/ecs_task_assume_role_policy.json")
+}

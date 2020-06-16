626abe62245f70d6a72d79abe755ac213c58d39d
ono <f25jzzydg@me.com>
2020-06-17 08:57:56 +0900

    Fix ECS resource name to use variable

--- ecs.tf
+++ ecs.tf
@@ -1,20 +1,17 @@
-resource "aws_ecs_cluster" "laravel-blog-sample-cluster" {
+resource "aws_ecs_cluster" "cluster" {
   name               = "$(var.project_name)-cluster"
 }
 
-resource "aws_ecs_service" "webapp-service" {
-  name               = "webapp-service"
-  cluster            = aws_ecs_cluster.laravel-blog-sample-cluster.id
-  # TaskDefinitionのarnを指定する
-  task_definition    = aws_ecs_task_definition.laravel-blog-sample-task.arn
-  # TaskDefinitionを実行するインスタンスの数
+resource "aws_ecs_service" "app-service" {
+  name               = "$(var.project_name)-service"
+  cluster            = aws_ecs_cluster.cluster.id
+  task_definition    = aws_ecs_task_definition.app.arn
   desired_count      = 1
-  # サービスを実行するタイプをEC2かFargateで指定する
   launch_type        = "EC2"
 }
 
-resource "aws_ecs_task_definition" "laravel-blog-sample-task" {
-  family                = "webapp-service"
+resource "aws_ecs_task_definition" "app" {
+  family                = "$(var.project_name)-app"
   container_definitions = file("container_definitions/service.json")
   task_role_arn         = aws_iam_role.ecs_task_role.arn
   network_mode          = "bridge"

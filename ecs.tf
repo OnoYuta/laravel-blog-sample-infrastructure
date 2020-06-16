90c5538e93c97c630ca2611d5e7fc13924248859
ono <f25jzzydg@me.com>
2020-06-17 08:51:48 +0900

    Add ECS service

--- ecs.tf
+++ ecs.tf
@@ -1,3 +1,21 @@
 resource "aws_ecs_cluster" "laravel-blog-sample-cluster" {
   name               = "$(var.project_name)-cluster"
 }
+
+resource "aws_ecs_service" "webapp-service" {
+  name               = "webapp-service"
+  cluster            = aws_ecs_cluster.laravel-blog-sample-cluster.id
+  # TaskDefinitionのarnを指定する
+  task_definition    = aws_ecs_task_definition.laravel-blog-sample-task.arn
+  # TaskDefinitionを実行するインスタンスの数
+  desired_count      = 1
+  # サービスを実行するタイプをEC2かFargateで指定する
+  launch_type        = "EC2"
+}
+
+resource "aws_ecs_task_definition" "laravel-blog-sample-task" {
+  family                = "webapp-service"
+  container_definitions = file("container_definitions/service.json")
+  task_role_arn         = aws_iam_role.ecs_task_role.arn
+  network_mode          = "bridge"
+}

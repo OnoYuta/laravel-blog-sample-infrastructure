935637cbf440ab17f717593c18f996129be536f2
ono <f25jzzydg@me.com>
2020-06-17 09:42:51 +0900

    Add cloudwatch aws logs

--- ecs.tf
+++ ecs.tf
@@ -1,17 +1,17 @@
 resource "aws_ecs_cluster" "cluster" {
-  name               = "$(var.project_name)-cluster"
+  name = "${var.project_name}-cluster"
 }
 
 resource "aws_ecs_service" "app-service" {
-  name               = "$(var.project_name)-service"
-  cluster            = aws_ecs_cluster.cluster.id
-  task_definition    = aws_ecs_task_definition.app.arn
-  desired_count      = 1
-  launch_type        = "EC2"
+  name            = "${var.project_name}-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = 1
+  launch_type     = "EC2"
 }
 
 resource "aws_ecs_task_definition" "app" {
-  family                = "$(var.project_name)-app"
+  family                = "${var.project_name}-app"
   container_definitions = file("container_definitions/service.json")
   task_role_arn         = aws_iam_role.ecs_task_role.arn
   network_mode          = "bridge"
